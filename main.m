4bfd1137f9087f2030c05eb11c9b215b57b0b8a6
danielctull <dt@danieltull.co.uk>
2010-09-26 17:36:46 +0100

    Initial commit.

---
+++ main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  DCTFoundation
+//
+//  Created by Daniel Tull on 26.09.2010.
+//  Copyright (c) 2010 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	// insert code here...
+	NSLog(@"Hello, World!");
+	[pool drain];
+    return 0;
+}
+
