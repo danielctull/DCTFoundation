27626b873241febfeaa90ff28ca07cb0a4c18b11
Daniel Tull <dt@danieltull.co.uk>
2011-04-03 14:33:25 +0100

    New project for a library

---
+++ DCTFoundationTests/DCTFoundationTests.h
@@ -0,0 +1,17 @@
+//
+//  DCTFoundationTests.h
+//  DCTFoundationTests
+//
+//  Created by Daniel Tull on 3.04.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DCTFoundationTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ DCTFoundationTests/DCTFoundationTests.m
@@ -0,0 +1,33 @@
+//
+//  DCTFoundationTests.m
+//  DCTFoundationTests
+//
+//  Created by Daniel Tull on 3.04.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import "DCTFoundationTests.h"
+
+
+@implementation DCTFoundationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DCTFoundationTests");
+}
+
+@end
