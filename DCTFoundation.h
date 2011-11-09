9e78c89e40b762d51da040870dff0790a8dc182c
Daniel Tull <dt@danieltull.co.uk>
2011-11-09 19:22:00 +0000

    Define all version numbers

--- DCTFoundation.h
+++ DCTFoundation.h
@@ -9,8 +9,20 @@
 #import <Foundation/Foundation.h>
 
 #ifndef dctfoundation
+
 #define dctfoundation_1_0     10000
+#define dctfoundation_1_0_1   10001
+
+#define dctfoundation_1_1     10100
+#define dctfoundation_1_1_1   10101
+#define dctfoundation_1_1_2   10102
+#define dctfoundation_1_1_3   10103
+#define dctfoundation_1_1_4   10104
+#define dctfoundation_1_1_5   10105
+
 #define dctfoundation_2_0     20000
 #define dctfoundation_2_0_1	  20001
+
 #define dctfoundation         dctfoundation_2_0_1
+
 #endif
